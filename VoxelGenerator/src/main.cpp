6585c156e074c895c3c49c3ed28302c4ec38bbe9
Fahd <fahd31256@gmail.com>
2023-04-22 09:06:38 +0100

    fix: add glad

--- VoxelGenerator/src/main.cpp
+++ VoxelGenerator/src/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include "glad.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if(!glfwInit()){
+        std::cout << "Failed to initialize glfw" << std::endl;
+    }
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Window", nullptr, nullptr);    
+    if(!window) {
+        std::cout << "Failed to create window" << std::endl;
+    }
+    glfwMakeContextCurrent(window);
+    if(!gladLoadGL()) {
+        std::cout << "Failed to initialize glad" << std::endl;
+    }
+
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glfwPollEvents();
+
+        
+
+        glfwSwapBuffers(window);
+    }
+
+
+
     return 0;
 }
